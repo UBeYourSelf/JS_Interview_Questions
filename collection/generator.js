9d1e71c7298142410e98578d4175954c2120f48f
jagadish594 <jagadish594@gmail.com>
2019-09-30 21:48:31 -0700

    Updated generators

--- collection/generator.js
+++ collection/generator.js
@@ -15,4 +15,22 @@ let ret = gen.next()
 console.log(ret)
 console.log(gen.next())
 
+//================================
+function * iterator(str){
+    let i = 0
+    let len = str.length
+    while(i<len){
+        yield(str.charAt(i))
+        i++
+    }   
+    yield(null)
+}
+
+var myiterator = iterator("test");
+console.log(myiterator.next().value) 
+console.log(myiterator.next().value)
+console.log(myiterator.next().value)
+console.log(myiterator.next().value)
+console.log(myiterator.next().value)
+
 
--- test.js
+++ test.js
@@ -1,2 +1,18 @@
 
+function * iterator(str){
+    let i = 0
+    let len = str.length
+    while(i<len){
+        yield(str.charAt(i))
+        i++
+    }   
+    yield(null)
+}
+
+var myiterator = iterator("test");
+console.log(myiterator.next().value) 
+console.log(myiterator.next().value)
+console.log(myiterator.next().value)
+console.log(myiterator.next().value)
+console.log(myiterator.next().value)
 
